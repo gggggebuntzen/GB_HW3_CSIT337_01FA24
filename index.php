e4ae23d175f44de195fdbe76e1f470bf170e8b2b
gggggebuntzen <buntzengeorge@gmail.com>
2024-11-24 23:28:42 -0500

    update

--- index.php
+++ index.php
@@ -1,31 +1,22 @@
+echo "<td>" . $row['MovieTitle'] . "</td>";
+                    echo "<td>" . $row['ReleaseDate'] . "</td>";
+                    echo "<td>" . $row['Genre'] . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <p>No movies found in the database.</p>
+    <?php endif; ?>
+
+    <!-- Add a link to the form to submit new data -->
+    <br>
+    <a href="form.php">Add a Movie</a>
+</body>
+</html>
+
 <?php
-// Connect to database
-include 'database.php'; // This file contains your DB connection info
-
-// Query to get all movies
-$query = "SELECT * FROM Movie";
-$result = mysqli_query($conn, $query);
+// Close the database connection
+$conn->close();
 ?>
-
-<table>
-    <tr>
-        <th>Movie Title</th>
-        <th>Release Date</th>
-        <th>Genre</th>
-        <th>Actions</th>
-    </tr>
-
-    <?php while ($row = mysqli_fetch_assoc($result)): ?>
-        <tr>
-            <td><?php echo $row['MovieTitle']; ?></td>
-            <td><?php echo $row['ReleaseDate']; ?></td>
-            <td><?php echo $row['Genre']; ?></td>
-            <td>
-                <!-- Edit Button -->
-                <a href="edit_movie.php?id=<?php echo $row['MovieID']; ?>">Edit</a> | 
-                <!-- Delete Button -->
-                <a href="delete_movie.php?id=<?php echo $row['MovieID']; ?>">Delete</a>
-            </td>
-        </tr>
-    <?php endwhile; ?>
-</table>
