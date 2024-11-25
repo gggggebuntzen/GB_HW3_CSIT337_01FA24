96ef2b6b7beac2d12d6c9083f5556cb5c643efaf
gggggebuntzen <buntzengeorge@gmail.com>
2024-11-24 21:54:16 -0500

    bfhd

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Or your custom MySQL username
-$password = "";      // Or your custom MySQL password
-$dbname = "MSU_Movies";  // Your database name
+$username = "mgs_user";  // Use the appropriate user
+$password = "pa55word";   // The password you set
+$dbname = "MSU_Movies";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
